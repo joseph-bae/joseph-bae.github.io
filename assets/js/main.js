acf41cedb067659c2685a8bb6cf8e25e738ef989
joseph-bae <joseph.bae@stonybrookmedicine.edu>
2021-01-16 23:24:34 -0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -99,7 +99,7 @@
 			$window.on('load', function() {
 
 				$('#two').poptrox({
-					caption: function($a) { return $a.next('p').text(); },
+					caption: function($a) { return $a.next('p'); },
 					overlayColor: '#2c2c2c',
 					overlayOpacity: 0.85,
 					popupCloserText: '',
